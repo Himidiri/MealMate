f66708b1a49a6ea461cdb21bc5eb859205cda785
Himidiri <himidirihimakanika@gmail.com>
2023-04-13 15:12:29 +0530

    Update the Main Page

--- app/src/main/java/com/example/mealmate/SplashScreen.kt
+++ app/src/main/java/com/example/mealmate/SplashScreen.kt
@@ -1,4 +1,24 @@
 package com.example.mealmate
 
-class SplashScreen {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashScreen : AppCompatActivity() {
+    private lateinit var image: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen_activity)
+
+        image = findViewById(R.id.logo)
+        image.alpha=0f
+        image.animate().setDuration(4000).alpha(1f).withEndAction{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+    }
 }

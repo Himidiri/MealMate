8ad5faac813b60521383eb8dc996348796466501
Himidiri <himidirihimakanika@gmail.com>
2023-05-14 02:12:20 +0530

    Implement Web service Part

---
+++ app/src/main/java/com/example/mealmate/SearchMealsByIngredient.kt
@@ -0,0 +1,11 @@
+package com.example.mealmate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchMealsByIngredient : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_meals_by_ingredient)
+    }
+}
